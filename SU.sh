5b2d435e1dcc76d48249f9d6a31e3c176b6e0ca6
ILICKTOES <81547139+ILICKTOES@users.noreply.github.com>
2021-09-06 01:26:06 -0400

    Update SU.sh

--- SU.sh
+++ SU.sh
@@ -15,13 +15,15 @@ yay -S pfetch
 sudo pacman -Rn manjaro-zsh-config mnajaro-system zsh && sudo pacman -Rn tlp 
 sudo pacman -S preload && sudo systemctl enable preload
 
-
 #  custom .dotfiles
+sudo rm -r /etc/pulse
+rm ~/.zshrc
 rm ~/.xinitrc
 cd ~/ 
 mkdir ~/.config/i3 && mkdir ~/.config/kitty
 mv ~/linuxconfig/pictures ~/ && mv ~/linuxconfig/config ~/.config/i3/config && mv ~/linuxconfig/kitty.conf ~/.config/kitty/kitty.conf 
-touch ~/.xinitrc && echo -e 'bash -c "nitrogen --restore" \nexec i3 >> ~/.xinitrc'
+touch ~/.xinitrc && echo -e 'bash -c "nitrogen --restore" \nexec i3' >> ~/.xinitrc
 rm -r ~/linuxconfig
+echo -e 'set -g -x fish_greeting '' \nla -h | lolcat' >> ~/.config/fish/config.fish
 cd ~/ && git clone https://github.com/oh-my-fish/oh-my-fish && cd ~/oh-my-fish && bin/install --offline
 
