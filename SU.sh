fa75dd61cc2fccfc1be867620684c44514fa5511
ILICKTOES <81547139+ILICKTOES@users.noreply.github.com>
2021-09-06 00:15:28 -0400

    Update SU.sh

--- SU.sh
+++ SU.sh
@@ -5,21 +5,22 @@
 #
 # main base install
 
-sudo pacman -Syyu & sudo pacman -S pipewire pipewire-pulse kitty fish i3 discord micro nitrogen dmenu cpupower htop pavucontrol && sudo systemctl enable cpupower
+sudo pacman -Sy && sudo pacman -S pipewire pipewire-pulse kitty fish i3 discord micro nitrogen dmenu cpupower htop pavucontrol && sudo systemctl enable cpupower
 
-yay -S brave && ttf-brutalist-mono
-yay -S lolcat && yay -S pfetch
+yay -S brave
+yay -S lolcat 
+yay -S ttf-brutalist-mono
+yay -S pfetch
 
-sudo pacman -Rn manjaro-zsh-config mnajaro-system  & sudo pacman -Rn tlp 
+sudo pacman -Rn manjaro-zsh-config mnajaro-system  && sudo pacman -Rn tlp 
 sudo pacman -S preload && sudo systemctl enable preload
-
-cd ~/ && git clone https://github.com/oh-my-fish/oh-my-fish && cd ~/oh-my-fish && bin/install --offline
 
 
 #  custom .dotfiles
-cd ~/ && git clone https://github.com/ILICKTOES/linuxconfig.git
+cd ~/ 
 mv ~/linuxconfig/pictures ~/ && mv ~/linuxconfig/config ~/.config/i3/config && mv ~/linuxconfig/kitty.conf ~/.config/kitty/kitty.conf 
-touch ~/.xinitrc & echo -e 'bash -c "nitrogen --restore" \nexec i3 >> ~/.xinitrc'
-
+touch ~/.xinitrc && echo -e 'bash -c "nitrogen --restore" \nexec i3 >> ~/.xinitrc'
+cd ~/ && git clone https://github.com/oh-my-fish/oh-my-fish && cd ~/oh-my-fish && bin/install --offline
 rm -r ~/linuxconfig
+ 
  echo -e 'done.'
