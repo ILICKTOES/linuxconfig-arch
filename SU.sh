4d5608041c3dbe1126a60e0a14399dbbc0a90a72
ILICKTOES <81547139+ILICKTOES@users.noreply.github.com>
2021-09-10 17:35:31 -0400

    Update SU.sh

--- SU.sh
+++ SU.sh
@@ -2,7 +2,7 @@
 sudo systemctl enable NetworkManager
 # main base install
 
-sudo pacman -Sy && sudo pacman -Syyu && sudo pacman -S pipewire pipewire-pulse kitty fish i3 discord micro nitrogen dmenu cpupower vivaldi htop pavucontrol xclip # xorg-server xorg-xinit xf86-input-libinput
+sudo pacman -Sy && sudo pacman -Syyu && sudo pacman -S pipewire pipewire-pulse kitty fish i3 sxiv steam discord micro nitrogen dmenu cpupower vivaldi htop pavucontrol xclip # xorg-server xorg-xinit xf86-input-libinput nvidia
 
 # build deps
 
