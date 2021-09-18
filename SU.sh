3e90f81adf1dd1e19a8de1399dfbebcfba9321c1
ILICKTOES <81547139+ILICKTOES@users.noreply.github.com>
2021-09-18 16:55:11 -0400

    Update SU.sh

--- SU.sh
+++ SU.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# sudo pacman -S yay base-devel git <<< use for vanialla arch
-# main packages
 
-sudo pacman -Sy && sudo pacman -Syyu && sudo gnome-boxes gamemode pacman -S pipewire pipewire-pulse kitty fish i3 sxiv steam discord micro nitrogen dmenu cpupower qutebrowser htop pavucontrol xclip xorg-server xorg-xinit xf86-input-libinput nvidia lib32-nvidia-utils 
+# main packages
+sudo pacman -Sy && sudo pacman -Syyu && sudo pacman -S base-devel git yay gnome-boxes gamemode pipewire pipewire-pulse kitty fish i3 sxiv steam discord micro nitrogen dmenu cpupower qutebrowser htop pavucontrol xclip xorg-server xorg-xinit xf86-input-libinput nvidia lib32-nvidia-utils 
 
 # build & cleanup
 
@@ -14,13 +13,11 @@ cd ~/ && git clone https://aur.archlinux.org/pfetch.git && cd ~/pfetch && makepk
 
 cd ~/ && git clone https://aur.archlinux.org/libstrangle.git && cd ~/libstrangle && makepkg -csi
 
-
-sudo pacman -Rn manjaro-zsh-config mnajaro-system zsh
-sudo pacman -Rn tlp nano vi
-
 sudo pacman -S preload && sudo systemctl enable preload
 
 sudo mv ~/linuxconfig-arch/grub /etc/default/grub
+
+sudo pacman -Rn tlp nano vi
 
 #  custom .dotfiles
 sudo rm -r /etc/pulse
@@ -36,6 +33,7 @@ touch ~/cpupower.service && echo -e '[Unit] \nDescription=CPU performance \n[Ser
 sudo mv ~/cpupower.service /etc/systemd/system/cpupower.service
 sudo systemctl daemon-reload
 sudo systemctl enable cpupower.service
-
+sudo mkdir ~/.steam/
+sudo mkdir ~/.steam/root/
+sudo mkdir ~/.steam/root/compatibilitytools.d
 cd ~/ && git clone https://github.com/oh-my-fish/oh-my-fish && cd ~/oh-my-fish && bin/install --offline
-
