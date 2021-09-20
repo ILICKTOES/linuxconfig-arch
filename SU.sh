7fa6fdf1f2f276f6bece0e45a6d6f434dd440e80
ILICKTOES <81547139+ILICKTOES@users.noreply.github.com>
2021-09-19 23:48:20 -0400

    Update SU.sh

--- SU.sh
+++ SU.sh
@@ -3,21 +3,23 @@
 # main packages
 sudo pacman -Sy && sudo pacman -Syyu && sudo pacman -S base-devel 
 sudo pacman -Sy
-sudo pacman -S kitty i3 sxiv nitrogen dmenu cpupower qutebrowser htop 
+sudo pacman -S kitty i3 sxiv nitrogen dmenu cpupower htop 
 sudo pacman -Sy
 sudo pacman -S pipewire pipewire-pulse pavucontrol
 sudo pacman -Sy
-sudo pacman -S steam discord gamemode gnome-boxes
+sudo pacman -S steam discord gamemode gnome-boxes qutebrowser
 sudo pacman -Syy
 # build & cleanup
 
-cd ~/ && git clone https://aur.archlinux.org/c-lolcat.git && cd ~/c-lolcat && makepkg -csi
+mkdir ~/git
 
-cd ~/ && git clone https://aur.archlinux.org/ttf-brutalist-mono.git && cd ~/ttf-brutalist-mono && makepkg -csi
+cd ~/git && git clone https://aur.archlinux.org/c-lolcat.git && cd ~/git/c-lolcat && makepkg -csi
 
-cd ~/ && git clone https://aur.archlinux.org/pfetch.git && cd ~/pfetch && makepkg -csi
+cd ~/git && git clone https://aur.archlinux.org/ttf-brutalist-mono.git && cd ~/git/ttf-brutalist-mono && makepkg -csi
 
-cd ~/ && git clone https://aur.archlinux.org/libstrangle.git && cd ~/libstrangle && makepkg -csi
+cd ~/git && git clone https://aur.archlinux.org/pfetch.git && cd ~/git/pfetch && makepkg -csi
+
+cd ~/git && git clone https://aur.archlinux.org/libstrangle.git && cd ~/git/libstrangle && makepkg -csi
 
 sudo mkdir /etc/default/grub
 sudo mv ~/linuxconfig-arch/grub /etc/default/grub
@@ -25,16 +27,14 @@ sudo mv ~/linuxconfig-arch/grub /etc/default/grub
 sudo pacman -S xclip xorg-server xorg-xinit xf86-input-libinput nvidia lib32-nvidia-utils
 
 #  custom .dotfiles
-rm ~/.xinitrc
-cd ~/ 
 mkdir ~/.config/i3 && mkdir ~/.config/kitty
-mv ~/linuxconfig-arch/pictures ~/ && mv ~/linuxconfig-arch/config ~/.config/i3/config && mv ~/linuxconfig-arch/kitty.conf ~/.config/kitty/kitty.conf 
+mv ~/linuxconfig-arch/pictures ~/git && mv ~/linuxconfig-arch/config ~/.config/i3/config && mv ~/linuxconfig-arch/kitty.conf ~/.config/kitty/kitty.conf 
 touch ~/.xinitrc && echo -e 'bash -c "nitrogen --restore" \nexec i3' >> ~/.xinitrc
-rm -r ~/linuxconfig-arch
 echo -e 'set -g -x fish_greeting '' \nla -h | lolcat' >> ~/.config/fish/config.fish
 touch ~/cpupower.service && echo -e '[Unit] \nDescription=CPU performance \n[Service] \nType=oneshot \ExecStart=/usr/bin/cpupower -c all frequency-set -g performance \n[Install] \nWantedBy=multi-user.target' >> ~/cpupower.service 
 sudo mv ~/cpupower.service /etc/systemd/system/cpupower.service
 sudo systemctl daemon-reload
 sudo systemctl enable cpupower.service
+rm -r ~/linuxconfig-arch
 sudo grub-mkconfig -o /boot/grub/grub.cfg 
-cd ~/ && git clone https://github.com/oh-my-fish/oh-my-fish && cd ~/oh-my-fish && bin/install --offline
+cd ~/git && git clone https://github.com/oh-my-fish/oh-my-fish && cd ~/git/oh-my-fish && bin/install --offline
