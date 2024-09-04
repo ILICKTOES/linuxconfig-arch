04448b462a4ec6c9d597df9e9a88cadd77093f26
ILICKTOES <81547139+ILICKTOES@users.noreply.github.com>
2024-09-04 14:16:10 -0400

    Update SU.sh

--- SU.sh
+++ SU.sh
@@ -4,13 +4,13 @@
 sudo pacman -Sy
 sudo pacman -Syyu 
 
-sudo pacman -S kitty i3 sxiv nitrogen dmenu htop thunar 
+sudo pacman -S kitty i3 nitrogen dmenu htop thunar 
 
 
 sudo pacman -S pipewire pipewire-pulse pipewire-media-session pavucontrol
 
 
-sudo pacman -S steam-runtime discord lutris gamemode lib32-gamemode wine wine-staging
+sudo pacman -S steam-runtime lutris gamemode lib32-gamemode wine wine-staging
 
 
 sudo pacman -S xclip xorg-server xorg-xinit xf86-input-libinput nvidia lib32-nvidia-utils nvidia-settings
@@ -26,7 +26,6 @@ cd ~/git && git clone https://aur.archlinux.org/ttf-brutalist-mono.git && cd ~/g
 sudo mkdir /etc/default/grub
 sudo mv ~/linuxconfig-arch/grub /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo pacman -Rns ninja python-beaker meson python-mako
 
 #  custom .dotfiles
 mkdir ~/.config/i3 && mkdir ~/.config/kitty 
