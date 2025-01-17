bb9415fe01829cfcf31cc0c6f45a66dfe9bca449
ILICKTOES <81547139+ILICKTOES@users.noreply.github.com>
2025-01-17 15:32:38 -0500

    Update SU.sh
    added 
    -browser = qutebrowser
    -terminal = simple terminal
    -nvidia-open
    -file manager = ranger
    -image viewer = Sxiv

--- SU.sh
+++ SU.sh
@@ -8,7 +8,7 @@ sudo pacman -Sy
 sudo pacman -Syyu 
 
 # Essentials
-sudo pacman -S kitty i3 nitrogen dmenu htop thunar qutebrowser 
+sudo pacman -S st i3 feh dmenu ranger sxiv qutebrowser
 
 # Pipewire Install
 sudo pacman -S pipewire pipewire-pulse pipewire-media-session pavucontrol
@@ -17,7 +17,7 @@ sudo pacman -S pipewire pipewire-pulse pipewire-media-session pavucontrol
 sudo pacman -S steam-runtime lutris gamemode lib32-gamemode wine wine-staging
 
 # Xorg and Graphics drivers
-sudo pacman -S xclip xorg-server xorg-xinit xf86-input-libinput nvidia lib32-nvidia-utils nvidia-settings
+sudo pacman -S xclip xorg-server xorg-xinit xf86-input-libinput nvidia-open lib32-nvidia-utils nvidia-settings
 
 # Pipewire Enable
 sudo systemctl --global pipewire pipewire-pulse pipewire-media-session
@@ -32,9 +32,6 @@ mkdir ~/git
 # font
 cd ~/git && git clone https://aur.archlinux.org/ttf-brutalist-mono.git && cd ~/git/ttf-brutalist-mono && makepkg -csi
 
-#Web browser
-cd ~/git && git clone https://aur.archlinux.org/brave-bin.git && cd ~/git/brave-bin && makepkg -csi
-
 #Grub
 sudo mkdir /etc/default/grub
 sudo mv ~/linuxconfig-arch/grub /etc/default/grub 
@@ -48,7 +45,7 @@ sudo grub-mkconfig -o /boot/grub/grub.cfg
 mkdir ~/.config/i3 && mkdir ~/.config/kitty 
 
 # config files
-mv ~/linuxconfig-arch/pictures ~/git && mv ~/linuxconfig-arch/config ~/.config/i3/config && mv ~/linuxconfig-arch/kitty.conf ~/.config/kitty/kitty.conf 
+mv ~/linuxconfig-arch/pictures ~/git && mv ~/linuxconfig-arch/config ~/.config/i3/config 
 touch ~/.xinitrc && echo -e '/usr/bin/pipewire & \n/usr/bin/pipewire-pulse & \nexec i3' >> ~/.xinitrc
 
 # Cleanup & Shell
